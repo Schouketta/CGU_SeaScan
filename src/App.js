cc956d6952047fb54c93a0863fefe8878b1a961e
Schouketta <sarah.le@live.fr>
2022-05-03 11:44:11 +0200

    page cgu

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="CGU_div" style={{ minHeight: '100%' }}>
+      <header className="CGU_title">
+        <h1>
+        Conditions générales de SeaScan by Sea Life
+        </h1>
       </header>
+        <p>
+        Les présentes Conditions Générales régissent les conditions dans lesquelles vous pouvez accéder et utiliser les différents services disponibles. Tout utilisateur des services proposés par SeaScan by Sea Life accepte les présentes conditions générales.
+        L'application SeaScan by Sea Life recueille très peu de données sur l'utilisateur, car elle ne nécessite pas de compte d'aucune sorte et tous les enregistrements sont stockés uniquement sur l'appareil mobile de l'utilisateur. SeaScan by Sea Life est une application qui ne nécessite pas de connexion internet pour fonctionner.
+        </p>
+
+      <h3>Principale fonctionnalité de SeaScan by Sea Life</h3>
+        <p>Identifier une espèce marine avec un flux d’images caméra. Le flux correspond à une ou plusieurs images de l’espèce à identifier.Ces données ne sont pas stockées, et permettent uniquement la reconnaissance d’une espèce marine sur le moment. 
+        A l’avenir, il est possible que ces données soient partiellement enregistrées pour permettre de suivre l'évolution des performances de notre algorithme de reconnaissance dans le temps, et d'améliorer ses performances. Ces conditions générales seront mises à jour en conséquence.
+        </p>
+      <h3>Limitation de responsabilité</h3>
+        <p>Vous reconnaissez avoir été informé et acceptez le fait que vous utilisez SeaScan by Sea Life à vos propres risques. En aucun cas, SeaScan by Sea Life ne sera responsable des dommages directs ou indirects, notamment des préjudices matériels ou des pertes de données liés à l'accès ou à l'utilisation de cette application. Les contenus de SeaScan by Sea Life sont affichés sans aucune garantie de quelque nature que ce soit. Vous êtes entièrement responsable de tout dommage causé à votre appareil mobile ou de toute perte de données qui pourrait survenir à la suite de l'utilisation de l'application.
+        En aucun cas SeaScan by Sea Life ne sera responsable de l'exactitude des résultats obtenus par l'utilisation de son algorithme de reconnaissance. SeaScan by Sea Life ne sera en aucun cas responsable des dommages directs ou indirects qui pourraient survenir à la suite de l'utilisation de l'application.
+        </p>
+       
     </div>
   );
 }
